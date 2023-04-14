bb3e53605dfb21753ab782ff05425dcf4f3726fb
Prashant <prashant.p@simformsolutions.com>
2023-04-14 17:32:55 +0530

    useRef hook

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Hooks from './components/Hooks';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Hooks topic='Hooks'/>
     </div>
   );
 }
---
+++ hooks/src/components/Useref.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function Useref() {
+    const [name, setName] = useState('');
+    const screenRender = useRef(1)
+    const refVal = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        screenRender.current = screenRender.current + 1
+    }, [name])
+
+    function focus() {
+        refVal.current?.focus()
+    }
+
+    return (
+        <>
+            <h1>useRef</h1>
+            <p>useRef is a React Hook that lets you reference a value that’s not needed for rendering.</p>
+            <p>Use 1 </p>
+            <input type="text" onChange={e => setName(e.target.value)} />
+            <p>{`You typed ${name}`}</p>
+            <p>{`Screen rendered ${screenRender.current} times`}</p>
+
+            <p>Use 2 </p>
+            <input ref={refVal} type="text" onChange={e => setName(e.target.value)} />
+            <p>{`You typed ${name}`}</p>
+            <button onClick={focus}>Focus</button>
+        </>
+
+
+    )
+}
